f9f705bc940654cc92629d9086a0ab57bbeb9204
purushottamparakh <puruparakh@gmail.com>
2020-05-18 22:57:21 +0530

    BST code, self = None works

---
+++ main.py
@@ -0,0 +1,15 @@
+from BinarySearchTree import BinarySearchTree
+from Node import Node
+
+BST = BinarySearchTree(6)
+BST.insert(3)
+BST.insert(2)
+BST.insert(4)
+BST.insert(-1)
+BST.insert(1)
+BST.insert(-2)
+BST.insert(8)
+BST.insert(7)
+
+BST.delete(4)
+
